877ff9b3bcb30e621f54ba5835cd41303d01908a
kristiyan-kiryakov <kristiyan.kiryakov@sumup.com>
2025-07-07 00:35:55 +0300

    implement round-robin scheduling for task management and enhance worker selection logic

--- manager/manager.go
+++ manager/manager.go
@@ -10,6 +10,8 @@ import (
 	"github.com/google/uuid"
 	"log"
 	"net/http"
+	"orchestrator-in-go/node"
+	"orchestrator-in-go/scheduler"
 	"orchestrator-in-go/task"
 	"orchestrator-in-go/worker"
 	"strings"
@@ -24,20 +26,8 @@ type Manager struct {
 	WorkerTaskMap map[string][]uuid.UUID
 	TaskWorkerMap map[uuid.UUID]string
 	LastWorker    int
-}
-
-func (m *Manager) SelectWorker() string {
-	var newWorker int
-
-	if m.LastWorker+1 < len(m.Workers) {
-		newWorker = m.LastWorker + 1
-		m.LastWorker++
-	} else {
-		newWorker = 0
-		m.LastWorker = 0
-	}
-
-	return m.Workers[newWorker]
+	WorkerNodes   []*node.Node
+	Scheduler     scheduler.Scheduler
 }
 
 func (m *Manager) updateTasks() {
@@ -82,30 +72,51 @@ func (m *Manager) updateTasks() {
 
 func (m *Manager) SendWork() {
 	if m.Pending.Len() > 0 {
-		w := m.SelectWorker()
-
 		e := m.Pending.Dequeue()
 		te := e.(task.TaskEvent)
-		t := te.Task
-		log.Printf("Pulled %v off pending queue\n", t)
-
 		m.EventDb[te.ID] = &te
-		m.WorkerTaskMap[w] = append(m.WorkerTaskMap[w], te.Task.ID)
-		m.TaskWorkerMap[t.ID] = w
+		log.Printf("Pulled %v off pending queue", te)
+
+		taskWorker, ok := m.TaskWorkerMap[te.Task.ID]
+		if ok {
+			persistedTask := m.TaskDb[te.Task.ID]
+			if te.State == task.Completed &&
+				task.ValidStateTransition(persistedTask.State, te.State) {
+				m.stopTask(taskWorker, te.Task.ID.String())
+				return
+			}
+
+			log.Printf(
+				"invalid request: existing task %s is in state %v and cannot transition to the completed state",
+				persistedTask.ID.String(), persistedTask.State)
+			return
+		}
+
+		t := te.Task
+		w, err := m.SelectWorker(t)
+		if err != nil {
+			log.Printf("error selecting worker for task %s: %v", t.ID, err)
+			return
+		}
+
+		log.Printf("[manager] selected worker %s for task %s", w.Name, t.ID)
+
+		m.WorkerTaskMap[w.Name] = append(m.WorkerTaskMap[w.Name], te.Task.ID)
+		m.TaskWorkerMap[t.ID] = w.Name
 
 		t.State = task.Scheduled
 		m.TaskDb[t.ID] = &t
 
 		data, err := json.Marshal(te)
 		if err != nil {
-			log.Printf("Unable to marshal task object: %v.\n", t)
+			log.Printf("Unable to marshal task object: %v.", t)
 		}
 
-		url := fmt.Sprintf("http://%s/tasks", w)
+		url := fmt.Sprintf("http://%s/tasks", w.Name)
 		resp, err := http.Post(url, "application/json", bytes.NewBuffer(data))
 		if err != nil {
-			log.Printf("Error connecting to %v: %v\n", w, err)
-			m.Pending.Enqueue(te)
+			log.Printf("[manager] Error connecting to %v: %v", w, err)
+			m.Pending.Enqueue(t)
 			return
 		}
 
@@ -117,7 +128,7 @@ func (m *Manager) SendWork() {
 				fmt.Printf("Error decoding response: %s\n", err.Error())
 				return
 			}
-			log.Printf("Response error (%d): %s\n", e.HTTPStatusCode, e.Message)
+			log.Printf("Response error (%d): %s", e.HTTPStatusCode, e.Message)
 			return
 		}
 
@@ -127,7 +138,8 @@ func (m *Manager) SendWork() {
 			fmt.Printf("Error decoding response: %s\n", err.Error())
 			return
 		}
-		log.Printf("%#v\n", t)
+		w.TaskCount++
+		log.Printf("[manager] received response from worker: %#v\n", t)
 	} else {
 		log.Println("No work in the queue")
 	}
@@ -137,14 +149,28 @@ func (m *Manager) AddTask(te task.TaskEvent) {
 	m.Pending.Enqueue(te)
 }
 
-func New(workers []string) *Manager {
+func New(workers []string, schedulerType string) *Manager {
 	taskDb := make(map[uuid.UUID]*task.Task)
 	eventDb := make(map[uuid.UUID]*task.TaskEvent)
 	workerTaskMap := make(map[string][]uuid.UUID)
 	taskWorkerMap := make(map[uuid.UUID]string)
 
+	var nodes []*node.Node
 	for worker := range workers {
 		workerTaskMap[workers[worker]] = []uuid.UUID{}
+
+		nAPI := fmt.Sprintf("http://%v", workers[worker])
+		n := node.NewNode(workers[worker], nAPI, "worker")
+		nodes = append(nodes, n)
+	}
+
+	var s scheduler.Scheduler
+
+	switch schedulerType {
+	case "roundrobin":
+		s = &scheduler.RoundRobin{Name: "roundrobin"}
+	default:
+		s = &scheduler.RoundRobin{Name: "roundrobin"}
 	}
 
 	return &Manager{
@@ -154,6 +180,8 @@ func New(workers []string) *Manager {
 		EventDb:       eventDb,
 		WorkerTaskMap: workerTaskMap,
 		TaskWorkerMap: taskWorkerMap,
+		WorkerNodes:   nodes,
+		Scheduler:     s,
 	}
 }
 
@@ -296,4 +324,41 @@ func (m *Manager) restartTask(t *task.Task) {
 		return
 	}
 	log.Printf("%#v\n", t)
+}
+
+func (m *Manager) SelectWorker(t task.Task) (*node.Node, error) {
+	candidates := m.Scheduler.SelectCandidateNodes(t, m.WorkerNodes)
+	if candidates == nil {
+		msg := fmt.Sprintf("No avialable candidates match resources request for task %v", t.ID)
+		err := errors.New(msg)
+		return nil, err
+	}
+	scores := m.Scheduler.Score(t, candidates)
+	if scores == nil {
+		return nil, fmt.Errorf("no scores returned to task %v", t)
+	}
+	selectNode := m.Scheduler.Pick(scores, candidates)
+
+	return selectNode, nil
+}
+
+func (m *Manager) stopTask(worker string, taskID string) {
+	client := &http.Client{}
+	url := fmt.Sprintf("http://%s/tasks/%s", worker, taskID)
+	req, err := http.NewRequest("DELETE", url, nil)
+	if err != nil {
+		log.Printf("error creating request to delete task %s : %v\n", taskID, err)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Printf("error connecting to worker at %s: %v\n", url, err)
+	}
+
+	if resp.StatusCode != 204 {
+		log.Printf("Error sending request: %v\n", err)
+		return
+	}
+
+	log.Printf("task %s has been scheduled to be stopped", taskID)
 }
