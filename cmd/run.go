51d891d74d24dffc20dc3dce3b9de32b5c85367f
kristiyan-kiryakov <kristiyan.kiryakov@sumup.com>
2025-07-08 14:01:19 +0300

    update run command description for clarity

--- cmd/run.go
+++ cmd/run.go
@@ -31,13 +31,10 @@ func fileExists(filename string) bool {
 // runCmd represents the run command
 var runCmd = &cobra.Command{
 	Use:   "run",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Run a new task",
+	Long: `cube run command.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+The run command starts a new task.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		manager, _ := cmd.Flags().GetString("manager")
 		filename, _ := cmd.Flags().GetString("filename")
